9d71018102f73823551dcd3db2d0c082640c0043
amirgi73 <amir.gi73@gmail.com>
2019-02-14 13:11:08 +0330

    fixing README.MD
    Signed-off-by: amirgi73 <amir.gi73@gmail.com>

--- spotifyapi.py
+++ spotifyapi.py
@@ -17,13 +17,15 @@ else:
     print(f"Usage: {sys.argv[0]} username")
     sys.exit()
 
-token = util.prompt_for_user_token(username, scope, CLIENT_ID, CLIENT_SECRET, REDIRECT_URL)
+token = util.prompt_for_user_token(
+    username, scope, CLIENT_ID, CLIENT_SECRET, REDIRECT_URL)
 tracks = []
 if token:
     spo = spotipy.Spotify(auth=token)
     # the user_playlist_tracks function gets two parametrs: 'user' and 'playlist'
     # which can be extracted from the playlist uri
-    results = spo.user_playlist_tracks('bnotmjg1ue4zu2d06ovib5sd6', '4ybqmPofUM18af8BtinhV9')
+    results = spo.user_playlist_tracks(
+        'bnotmjg1ue4zu2d06ovib5sd6', '4ybqmPofUM18af8BtinhV9')
 
     for item in results['items']:
         cover = item['track']['album']['images'][0]['url']
@@ -32,9 +34,11 @@ if token:
         for artist in item['track']['artists']:
             track_artists.append(artist['name'])
         track_album = item['track']['album']['name']
-        tracks.append({'name': track_name, 'artists': track_artists, 'album': track_album, 'cover_url': cover})
+        tracks.append({'name': track_name, 'artists': track_artists,
+                       'album': track_album, 'cover_url': cover})
 else:
     print(f"can't get token for {username}")
+    logger.error(f"can't get token for {username}")
 
 if __name__ == "__main__":
     for track in tracks:
@@ -51,7 +55,8 @@ if __name__ == "__main__":
                 audiofile.tag.title = title
                 audiofile.tag.artist = f"{','.join(artists)}"
                 audiofile.tag.album = album
-                audiofile.tag.images.set(3, open('cover.jpg', 'rb').read(), 'image/jpeg')
+                audiofile.tag.images.set(
+                    3, open('cover.jpg', 'rb').read(), 'image/jpeg')
                 audiofile.tag.save()
             except OSError as e:
                 logger.error(f"Couldn't set mp3 tags: {e}")
